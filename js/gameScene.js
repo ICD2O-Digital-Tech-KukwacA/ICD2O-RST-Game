5febe8480651467e1348f97f1619e9da2ab3f5d2
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-05-30 14:51:56 +0000

    Test run 11

--- js/gameScene.js
+++ js/gameScene.js
@@ -9,6 +9,7 @@
 class GameScene extends Phaser.Scene {
     constructor() {
         super({ key: 'gameScene' });
+        
         this.background = null;
         this.ship = null;
     }
@@ -22,12 +23,13 @@ class GameScene extends Phaser.Scene {
         console.log('Game Scene');
         // Images
         this.load.image('planetBackground', './assets/planetBackground.jpg');
-        this.load.image('ship', './assets/spaceShip.png');
+        this.load.image('ship', './assets/spaceShip.jpg');
     }
 
     create(data) {
         this.background = this.add.image(0, 0, 'planetBackground').setScale(2.0);
         this.background.setOrigin(0, 0);
+        
         this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship');
     }
 
@@ -36,14 +38,14 @@ class GameScene extends Phaser.Scene {
         const keyRightObj = this.input.keyboard.addKey(RIGHT);
         
         if (keyLeftObj.isDown === true) {
-            this.ship.x -= 20;
+            this.ship.x = this.ship.x - 20;
             if (this.ship.x < 0) {
                 this.ship.x = 0; // Prevent ship from going off the left edge
             }
         }
 
         if (keyRightObj.isDown === true) {
-            this.ship.x += 20;
+            this.ship.x = this.ship.x + 20;
             if (this.ship.x > 1920) {
                 this.ship.x = 1920; // Prevent ship from going off the right edge
             }
