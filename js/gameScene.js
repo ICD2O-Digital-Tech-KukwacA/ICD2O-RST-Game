20d4ac6d81b30d22606dc93dcdd7dc119725d0eb
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-06-03 16:57:34 +0000

    RST

--- js/gameScene.js
+++ js/gameScene.js
@@ -11,7 +11,7 @@ class GameScene extends Phaser.Scene {
     //Create an alien
     createAlien() {
         // Only a new alien if there are fewer than 25 aliens
-        if (this.alienGroup.countActive(true) <= 25) {
+        if (this.alienGroup.countActive(true) <= 10) {
         const alienXLocation = Math.floor(Math.random() * 1920) + 1;
         let alienXVelocity = Math.floor(Math.random() * 50) + 1; // Random speed between 50 and 1
         alienXVelocity *= Math.round(Math.random()) ? 1 : -1; // Randomly set direction to left or right
@@ -58,7 +58,7 @@ class GameScene extends Phaser.Scene {
         this.fireMissile = false; // Reset fireMissile to false at the start of the game
         this.isGameOver = false; // Reset isGameOver to false at the start of the game
         this.score = 0;
-        this.background = this.add.image(0, 0, 'newBackgroundImage').setScale(5.0);
+        this.background = this.add.image(0, 0, 'newBackgroundImage').setScale(10.0);
         this.background.setOrigin(0, 0);
 
         this.scoreText = this.add.text(10, 10, 'Score: ' + this.score.toString(), this.scoreTextStyle);
